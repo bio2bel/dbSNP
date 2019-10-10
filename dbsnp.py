f636dc1789e3dfaecfac7318e6f04ecab12a8e18
laurendelong21 <54819826+laurendelong21@users.noreply.github.com>
2019-10-10 17:17:52 +0200

    Added dbsnp.py with only mutations affecting genes
    This version has addressed all of the issues mentioned (Feel free to look at the attached screenshot). 
    Some questions I have for you:
    - Do you like the proteins in the list like that?
    -Each json entry has a baseline comparison for dna_change that usually ends in = because it's what would happen if the mutation was not present. Do you want me to take these out, or are these entries useful for our end goal?
    -Do you want 'rs' inserted in front of the dbsnp_id?

--- dbsnp.py
+++ dbsnp.py
@@ -5,6 +5,7 @@ import bz2
 import re
 from dataclasses import dataclass
 from typing import List
+import itertools
 
 
 @dataclass
@@ -55,91 +56,63 @@ def main():
             rs_obj = json.loads(line.decode('utf-8'))
             dbsnp_id = rs_obj['refsnp_id']  # the dbsnp id
 
-            all_ann_list_raw = rs_obj['primary_snapshot_data'][
-                'allele_annotations']  # these are the assembly annotations
+            all_ann_list_raw = rs_obj['primary_snapshot_data']['allele_annotations']  # these are the assembly annotations
 
-            # Here I parse through each hgvs entry and assign it to either a nuc. change or a.a. change
-            hgvs_entries = rs_obj['primary_snapshot_data']['placements_with_allele']
-            dna_change = ''
-            aa_change = ''
-            rna_change = ''
-            for entry in hgvs_entries:
-                seq_type = entry['placement_annot']['seq_type']
-                for variant in entry['alleles']:
-                    hgvs = variant['hgvs']
-                    hgvs = re.split(":[cgmnopr].", hgvs)
-                    if len(hgvs) > 1:
-                        if (seq_type == 'refseq_chromosome') or (seq_type == 'refseq_genomic'):
-                            dna_change = hgvs[1]
-                        elif seq_type == 'refseq_prot':
-                            aa_change = hgvs[1]
-                        elif seq_type == 'refseq_mrna':
-                            rna_change = hgvs[1]
+            if len(all_ann_list_raw) >= 2:  # if it has sufficient info
+                assembl_ann_list_raw = all_ann_list_raw[1]['assembly_annotation']  # against each assembly
+                if len(assembl_ann_list_raw) != 0: # if it contains gene info
+                    gene_list_raw = assembl_ann_list_raw[0]['genes']  # and each of the genes affected within each assembly
+                    if len(gene_list_raw) > 0:
+                        # Here I start extracting gene info:
+                        for x, y, z in itertools.product(range(len(all_ann_list_raw)),
+                                                         range(len(assembl_ann_list_raw)),
+                                                         range(len(gene_list_raw))):
+                            assembly_id = all_ann_list_raw[x]['assembly_annotation'][y]['seq_id']
+                            gene_name = all_ann_list_raw[x]['assembly_annotation'][y]['genes'][z]['name']
+                            gene_abbr = all_ann_list_raw[x]['assembly_annotation'][y]['genes'][z]['locus']
+                            gene_id = all_ann_list_raw[x]['assembly_annotation'][y]['genes'][z]['id']
+                            rna_list_raw = all_ann_list_raw[x]['assembly_annotation'][y]['genes'][z]['rnas']
 
-                    if len(all_ann_list_raw) < 2:  # for insufficient information
+                            proteins = []
+                            for t in range(len(rna_list_raw)):
+                                if 'product_id' in rna_list_raw[t]:
+                                    protein = rna_list_raw[t][
+                                        'product_id']  # the protein affected by the mutation
+                                    proteins.append(protein)
 
-                        snp_infos = SnpInfo(dbsnp_id,
-                                            '',
-                                            dna_change,
-                                            aa_change,
-                                            rna_change,
-                                            '',
-                                            '',
-                                            '',
-                                            []
-                                            )
-                        snp2csv(snp_infos, output)
+                            # Here I parse through each hgvs entry and assign it to either a nuc. change or a.a. change
+                            hgvs_entries = rs_obj['primary_snapshot_data']['placements_with_allele']
+                            dna_change = aa_change = rna_change = ''
+                            for num, entry in enumerate(hgvs_entries):
+                                seq_type = entry['placement_annot']['seq_type']
+                                for variant in entry['alleles']:
+                                    assembly_id2 = hgvs_entries[num]['seq_id']
+                                    hgvs = variant['hgvs']
+                                    hgvs = re.split(":[cgmnopr].", hgvs)
+                                    if len(hgvs) > 1:
+                                        if (seq_type == 'refseq_chromosome') or (seq_type == 'refseq_genomic'):
+                                            dna_change = hgvs[1]
+                                        elif seq_type == 'refseq_prot':
+                                            aa_change = hgvs[1]
+                                        elif seq_type == 'refseq_mrna':
+                                            rna_change = hgvs[1]
 
-                    else:
-                        assembl_ann_list_raw = all_ann_list_raw[1]['assembly_annotation']  # against each assembly
-                        if len(assembl_ann_list_raw) == 0:
-                            snp_infos = SnpInfo(dbsnp_id,
-                                                '',
-                                                dna_change,
-                                                aa_change,
-                                                rna_change,
-                                                '',
-                                                '',
-                                                '',
-                                                []
-                                                )
-                            snp2csv(snp_infos, output)
-                        else:
+                                    if assembly_id2 == assembly_id: # if this information corresponds with one of our gene entries...
+                                        snp_infos = SnpInfo(dbsnp_id,
+                                                            assembly_id,
+                                                            dna_change,
+                                                            aa_change,
+                                                            rna_change,
+                                                            gene_name,
+                                                            gene_abbr,
+                                                            gene_id,
+                                                            proteins
+                                                            )
 
-                            gene_list_raw = assembl_ann_list_raw[0][
-                                'genes']  # and each of the genes affected within each assembly
-                            if len(gene_list_raw) > 0:
-                                for x in range(len(all_ann_list_raw)):
-                                    for y in range(len(assembl_ann_list_raw)):
-                                        for z in range(len(gene_list_raw)):
-                                            gene_name = all_ann_list_raw[x]['assembly_annotation'][y]['genes'][z][
-                                                'name']
-                                            gene_abbr = all_ann_list_raw[x]['assembly_annotation'][y]['genes'][z][
-                                                'locus']
-                                            gene_id = all_ann_list_raw[x]['assembly_annotation'][y]['genes'][z]['id']
-                                            assembly_id = all_ann_list_raw[x]['assembly_annotation'][y]['seq_id']
-                                            rna_list_raw = all_ann_list_raw[x]['assembly_annotation'][y]['genes'][z][
-                                                'rnas']
+                                        snp2csv(snp_infos, output)
 
-                                            proteins = []
-                                            for t in range(len(rna_list_raw)):
-                                                if 'product_id' in rna_list_raw[t]:
-                                                    protein = rna_list_raw[t][
-                                                        'product_id']  # the protein affected by the mutation
-                                                    proteins.append(protein)
-
-                                            snp_infos = SnpInfo(dbsnp_id,
-                                                                assembly_id,
-                                                                dna_change,
-                                                                aa_change,
-                                                                rna_change,
-                                                                gene_name,
-                                                                gene_abbr,
-                                                                gene_id,
-                                                                proteins
-                                                                )
-
-                                            snp2csv(snp_infos, output)
+                                    else:
+                                        continue
 
     print("Finished writing files to CSV.")
 
